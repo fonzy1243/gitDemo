5b2307914accad034c2ee41e7218aef24792f8fd
Fonzy Fermin <fonzy.fermin@gmail.com>
2023-03-09 16:42:53 +0800

    Added first printf

--- main.c
+++ main.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main()
+{
+    printf("Line 1\n");
+
+    // Put line 2 here
+
+    // Put line 3 here 
+
+    // Put line 4 here
+
+    // Put line 5 here
+
+    // Put line 6 here
+}
